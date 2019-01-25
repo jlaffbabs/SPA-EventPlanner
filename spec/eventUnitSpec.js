b7d83cfafb5fa99d683e7ba6bcb604903f8c9f36
Jacques <jacques.laffy@gmail.com>
2019-01-25 11:04:39 +0000

    Added and passed test for event to have a city

--- spec/eventUnitSpec.js
+++ spec/eventUnitSpec.js
@@ -1,16 +1,18 @@
 describe('Event', () => {
+  let event = new Event("JAG's BBQ: Fun for everyone", "10/10/2019", "08:00", "London")
   it('should store a description', () => {
-    let event = new Event("JAG's BBQ: Fun for everyone")
     expect(event.description).toBe("JAG's BBQ: Fun for everyone")
-  })
+  });
 
   it('should store a date', () => {
-    let event = new Event("JAG's BBQ: Fun for everyone", "10/10/2019")
     expect(event.date).toBe("10/10/2019")
-  })
+  });
 
   it('should store a time', () => {
-    let event = new Event("JAG's BBQ: Fun for everyone", "10/10/2019", "08:00")
     expect(event.time).toBe("08:00")
-  })
-})
+  });
+
+  it('should store a city', () => {
+    expect(event.city).toBe("London")
+  });
+});
--- src/event.js
+++ src/event.js
@@ -1,16 +1,17 @@
 class Event {
-  constructor(description, date, time) {
+  constructor(description, date, time, city) {
     this.description = description;
     this.date = date;
     this.time = time;
-  }
+    this.city = city;
+  };
 
   elementToDisplay() {
   	let div = document.createElement('div')
   	div.innerText = `${this.description} on ${this.date} at ${this.time}`
   	return div;
-  }
+  };
 
   
-}
+};
 
