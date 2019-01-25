5fe5a0bc9ec883efce62cb7e717f5eb602a5a696
Jacques <jacques.laffy@gmail.com>
2019-01-25 14:29:37 +0000

    Textboxes clear upon submitting an event

--- interface.js
+++ interface.js
@@ -4,10 +4,12 @@ window.addEventListener('load', function() {
 
   let submitButton = document.getElementById('button');
   submitButton.addEventListener('click', function(){
-    let eventDescription = document.getElementById('desc').value;
-    let eventDate = document.getElementById('date').value;
-    let eventTime = document.getElementById('time').value;
-    let eventNew = new Event(eventDescription, eventDate, eventTime);
+    let eventDescription = document.getElementById('desc')
+    let eventDate = document.getElementById('date')
+    let eventTime = document.getElementById('time')
+    let eventNew = new Event(eventDescription.value, eventDate.value, eventTime.value);
+    eventDescription.value = eventDate.value = eventTime.value = "";
+    console.log(eventDescription.value);
     calendar.add(eventNew);
     display() ;
   });
